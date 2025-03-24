2fbe8bfccbae01d4a5bd21a816c3b58dc2ce6616
Esha <eshasdalal@gmail.com>
2025-03-23 20:38:54 -0400

    small fix

--- src/main/java/ca/mcmaster/se2aa4/island/team38/Stage.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team38/Stage.java
@@ -1,5 +1,5 @@
 package ca.mcmaster.se2aa4.island.team38;
 
 public enum Stage {
-        START, TURN, ECHO, FIND_ISLAND, SCAN, SCAN_AND_TURN, OUT_OF_RANGE, LOCATE_2, SCAN_2, SCAN_TURN_2, TEST
+        START, TURN, ECHO, FIND_ISLAND, SCAN, SCAN_AND_TURN, OUT_OF_RANGE;
     }
