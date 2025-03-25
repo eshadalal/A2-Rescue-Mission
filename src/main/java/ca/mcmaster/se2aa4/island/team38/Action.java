ed90e0b4f9ebf4c7ddd14eea1263c4b180b81015
Esha <eshasdalal@gmail.com>
2025-03-24 23:18:03 -0400

    final code

--- src/main/java/ca/mcmaster/se2aa4/island/team38/Action.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team38/Action.java
@@ -1,5 +1,6 @@
 package ca.mcmaster.se2aa4.island.team38;
 
+// enum of possible actions 
 public enum Action {
     FLY, TURNRIGHT, TURNLEFT, SCAN, STOP, ECHO, ECHOFORWARD, ECHORIGHT, ECHOLEFT;
 } 
